f7b636facd1785883faa0820bc0d74f2228daef1
erikaanne9 <erikaanne9@gmail.com>
2022-08-10 17:08:24 -0500

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Dictionary from "./Dictionary";
+
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">Coded by Erika Jones</footer>
+      </div>
     </div>
   );
 }
