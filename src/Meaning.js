273547f164cfbfd50614853d81ab734b1b96c1b5
erikaanne9 <erikaanne9@gmail.com>
2022-08-18 19:41:28 -0500

    added synonym

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,22 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech} </h3>
-      <p>{props.meaning.definitions[0].definition}</p>
-      <p>{props.meaning.definitions[0].example}</p>
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <h5>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
+            </h5>
+          </div>
+        );
+      })}
     </div>
   );
 }
